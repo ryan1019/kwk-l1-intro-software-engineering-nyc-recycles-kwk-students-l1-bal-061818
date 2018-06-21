c0fc9432306471062b8dcffdffc6e2fbda609bb9
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-21 18:49:13 +0000

    Done.

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,8 +1,8 @@
 ###### Code Your Solutions Here!!!
 
 #Your code should work with a hash that looks like the one below (in comments).
-#The specs (which call your tests) are using these exact hashes 
-# We haven't hard coded the hashes in the tests, 
+#The specs (which call your tests) are using these exact hashes
+# We haven't hard coded the hashes in the tests,
 #they're dynamically brought in from the NYC Open Data API
 
 # first_recycle_bin =  {
@@ -17,18 +17,33 @@
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
-
+def address(recyling)
+  recyling ["address"]
+end
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
-
+def borough(recyling)
+  recyling ["borough"]
+end
 #3. Define a method called `all_keys` that takes a hash as an argument.
-#This method should print out each key in the hash. 
+#This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
-
+def all_keys(recyling)
+  recyling.each
+end
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
-
+def all_data(recyling)
+  recyling.each do |x,y|
+    puts "#{x}"
+  end
+end
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+def key_and_data(recyling)
+  recyling.each do |x,y|
+    puts "The #{x} is #{y}".
+  end
+end  
