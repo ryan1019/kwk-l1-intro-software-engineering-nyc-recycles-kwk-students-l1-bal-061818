5d1ab2b0a78f40f4fca203d3f72516ef6585004c
ryan1019 <ryan.greathouse05@gmail.com>
2018-06-22 12:59:58 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -20,17 +20,23 @@
 def address(recyling)
   recyling ["address"]
 end
+end
+
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 def borough(recyling)
   recyling ["borough"]
 end
+end
+
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
 def all_keys(recyling)
   recyling.each
 end
+end
+
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
@@ -39,11 +45,14 @@ def all_data(recyling)
     puts "#{x}"
   end
 end
+
+
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
 def key_and_data(recyling)
   recyling.each do |x,y|
     puts "The #{x} is #{y}".
+
   end
-end  
+end
